c2fb6e553fd7fdcb77005c99f19cab064a96d066
Justin Sitter <justin@jaytria.com>
2019-03-21 22:50:03 -0700

    Refactor Header into component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+  render(){
+    return(
+      <header>
+        <img src="/imgs/public-domain-heart.png" alt="Without a heart, you'd have a hard time in the morning."/>
+        <h1>Heart Disease: The World's Leading Killer</h1>
+      </header>
+    );
+  };
+
+};
+
+export default Header;
