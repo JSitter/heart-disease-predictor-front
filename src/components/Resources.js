ea83d4ab3cbaf57ca5906eca3276b10e8d9aefdb
Justin Sitter <justin@jaytria.com>
2019-03-22 00:06:27 -0700

    Update layout in Resources

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -5,13 +5,14 @@ class Resources extends Component {
     return(
         <section className="long-descriptive img-float-left">
         <h2>What Kind of Title is This? One of a Kind</h2>
+        <img className='left' src='https://source.unsplash.com/350x500/?exercise' alt="Exercise is good for you."/>
         <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
         <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
-        <img src='https://source.unsplash.com/350x500/?exercise' alt="Exercise is good for you."/>
+
 
         <p className="entice-end">
         This text is telling you that there is no more content. It urges you to try a link some where. 
