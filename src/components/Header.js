ca4b787f8a5048fbd634f127cb83a21f16da74f0
Justin Sitter <justin@jaytria.com>
2019-03-22 00:04:40 -0700

    add css styling to Introduction component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
+import '../css/Header.css';
+
 class Header extends Component {
 
   render(){
     return(
       <header>
         <img src="/imgs/public-domain-heart.png" alt="Without a heart, you'd have a hard time in the morning."/>
+        
         <h1>Heart Disease: The World's Leading Killer</h1>
       </header>
     );
