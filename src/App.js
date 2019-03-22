d187c11c4cc803b6305a5bd9a5855221bcb618c0
Justin Sitter <justin@jaytria.com>
2019-03-21 17:08:08 -0700

    refactor css to css folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-
-import './App.css';
+import './css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <header>
+          <img src="/imgs/public-domain-heart.png" alt="Without a heart, you'd have a hard time in the morning."/>
+          <h1>Heart Disease: The World's Leading Killer</h1>
+        </header>
+        
 
       </div>
     );
