a10deb06da766fcf5284586b3abad2708f415289
Justin Sitter <justin@jaytria.com>
2019-03-21 22:59:12 -0700

    refactor resources section into component

---
+++ src/components/Resources.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Resources extends Component {
+  render(){
+    return(
+        <section className="long-descriptive img-float-left">
+        <h2>What Kind of Title is This? One of a Kind</h2>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <img src='https://source.unsplash.com/350x500/?exercise' alt="Exercise is good for you."/>
+
+        <p className="entice-end">
+        This text is telling you that there is no more content. It urges you to try a link some where. 
+        Perhaps to GitHub. 
+        Perhaps to Screen AI.
+        </p>
+
+        </section>
+    );
+  };
+};
+
+export default Resources;
