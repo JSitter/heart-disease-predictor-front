f3d586bc12dce22d0be626f70f7e6152f08375be
Justin Sitter <justin@jaytria.com>
2019-03-25 13:13:36 -0700

    update navbar with components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -29,11 +29,10 @@ class NavBar extends Component {
     return(
         <nav id='main-nav'>
           <div className="nav-wrapper">
-            <a href="#" className="brand-logo">Logo</a>
+            <a href="#" className="brand-logo">Heart Disease Prediction</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li><a href="sass.html">Sass</a></li>
-              <li><a href="badges.html">Components</a></li>
-              <li><a href="collapsible.html">JavaScript</a></li>
+              <li><a href="sass.html">Screen AI</a></li>
+            
             </ul>
           </div>
         </nav>
