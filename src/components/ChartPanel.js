448f823099e0d5f51af294d1330bb8b9d47a2a3a
Justin Sitter <justin@jaytria.com>
2019-03-24 21:55:13 -0700

    Add CSS stylesheet.

--- src/components/ChartPanel.js
+++ src/components/ChartPanel.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
+import '../css/ChartPanel.css';
 
 class Header extends Component {
 
   render(){
     return(
-        <section className="chart-panel">
-          <h2>World Statistics</h2>
-          <h3>Chart One Title</h3>
-          <div className="chart"></div>
-          <p className="chart-description">This is a description of what this chart is depicting. It is beautiful and stunning to behold.</p>
-          <h3>Chart Two Title</h3>
-          <div className="chart"></div>
-          <p className="chart-description">This is a description of what this chart is depicting. It is beautiful and stunning to behold.</p>
+        <section className='chart-panel'>
+          <div className='chart-container'>
+            <h2>World Statistics</h2>
+            <h3>Chart One Title</h3>
+            <div className='chart'></div>
+            <p className='chart-description'>This is a description of what this chart is depicting. It is beautiful and stunning to behold.</p>
+            <h3>Chart Two Title</h3>
+            <div className='chart'></div>
+            <p className='chart-description'>This is a description of what this chart is depicting. It is beautiful and stunning to behold.</p>
 
-          <h2>US Stastics</h2>
-          <h3>Chart One Title</h3>
-          <div className="chart"></div>
-          <p className="chart-description">This is a description of what this chart is depicting. It is beautiful and stunning to behold.</p>
-          <h3>Chart Two Title</h3>
-          <div className="chart"></div>
-          <p className="chart-description">This is a description of what this chart is depicting. It is beautiful and stunning to behold.</p>
+            <h2>US Stastics</h2>
+            <h3>Chart One Title</h3>
+            <div className='chart'></div>
+            <p className='chart-description'>This is a description of what this chart is depicting. It is beautiful and stunning to behold.</p>
+            <h3>Chart Two Title</h3>
+            <div className='chart'></div>
+            <p className='chart-description'>This is a description of what this chart is depicting. It is beautiful and stunning to behold.</p>
+          </div>
         </section>
     );
   };
