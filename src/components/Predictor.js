8afed5e5abbfcd512e03b030e598606ba5d44272
Justin Sitter <justin@jaytria.com>
2019-03-25 13:14:25 -0700

    add Predictor component

---
+++ src/components/Predictor.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Predictor extends Component {
+  render(){
+    return (
+      <div>
+        <p>hello!</p>
+      </div>
+    )
+  }
+}
+
+export default Predictor;
