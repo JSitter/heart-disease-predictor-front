94c2ae29478c520a4fbb7312110fa804260f98ea
Justin Sitter <justin@jaytria.com>
2019-03-24 20:55:28 -0700

    Add header to introduction section

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -8,6 +8,7 @@ class Introduction extends Component {
     return(
         <section className="long-descriptive">
         <img className='right' src='https://source.unsplash.com/350x500/?anatomical-heart' alt='Your heart is special.'/>
+          <h2>Heart Disease Things that may surprise you.</h2>
           <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
