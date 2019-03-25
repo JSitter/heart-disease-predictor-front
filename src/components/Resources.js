4277e8fd5a5f094bb63fd20a62153a2e61ed6298
Justin Sitter <justin@jaytria.com>
2019-03-25 13:12:15 -0700

    Update resources section with content

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -4,20 +4,28 @@ class Resources extends Component {
   render(){
     return(
         <section className="long-descriptive img-float-left">
-        <h2>What Kind of Title is This? One of a Kind</h2>
+        <h2>How Can I Reduce My Risk?</h2>
         <img className='left' src='https://source.unsplash.com/350x500/?exercise' alt="Exercise is good for you."/>
         <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        While heart disease is a major killer in the United States and around the world, the effects of heart disease aren't likely to be felt until around mid-life. Some risks of developing heart disease come from genetic factors, though there are ways to live a healthy life to reduce the risk of heart problems.
         </p>
+        <h3>Don't Smoke</h3>
         <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        Risk associated with heart disease are increased when you smoke. Quitting now will lower your risk of heart disease, stroke, and cancer.
         </p>
+        
+        <h3>Exercise Regularly</h3>
+        <p>Regular exercise helps maintain heart health with surprisingly little effort. As little as 30 minutes of aerobic activity several times per week is enough to reduce the risk of heart disease. When combined with a healthy diet, weight, and plenty of sleep, this is a great way to stay healthy for the long run</p>
+
+        <h3>Eat Healthy</h3>
+        <p>Another great way to reduce your risk of heart disease is to eat foods that are healthy for your. Keeping your weight down to a normal level will reduce the strain on your heart and keep it in prime condition longer.</p>
 
 
+
+        <h3>Check Out Additional Resources</h3>
         <p className="entice-end">
-        This text is telling you that there is no more content. It urges you to try a link some where. 
-        Perhaps to GitHub. 
-        Perhaps to Screen AI.
+        
+        There are many resources out there to help you improve your heart health. The <a href="https://www.mayoclinic.org/diseases-conditions/heart-disease/in-depth/heart-disease-prevention/art-20046502">Mayo Clinic</a> has a wonderful page that lists many of the ways to improve your health.
         </p>
 
         </section>
