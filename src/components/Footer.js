2d848c2cbf693066e5d8c42fa2ff9eb8a2c0fda0
Justin Sitter <justin@jaytria.com>
2019-03-24 22:36:44 -0700

    Add styles to footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../css/Footer.css';
 
 class Footer extends Component {
   render(){
@@ -7,24 +8,22 @@ class Footer extends Component {
         <div className="container">
           <div className="row">
             <div className="col l6 s12">
-              <h5 className="white-text">Footer Content</h5>
-              <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              <h5 >Support open Source</h5>
+              <p>
+                View the project on <a href="https://github.com/JSitter/heart-disease-predictor-front">
+                GitHub
+                </a>
+              </p>
             </div>
             <div className="col l4 offset-l2 s12">
-              <h5 className="white-text">Links</h5>
+              <h5>Links</h5>
               <ul>
-                <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                <li><a href="#!">Link 1</a></li>
+                <li><a href="#!">Link 2</a></li>
+                <li><a href="#!">Link 3</a></li>
+                <li><a href="#!">Link 4</a></li>
               </ul>
             </div>
-          </div>
-        </div>
-        <div className="footer-copyright">
-          <div className="container">
-          © 2014 Copyright Text
-          <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
           </div>
         </div>
       </footer>
