bc05f924b555c19ee21b26c8f51adc2b3b29aa80
Justin Sitter <justin@jaytria.com>
2019-05-16 00:45:26 -0700

    Update fetch with no-cors

--- src/components/Predictor.js
+++ src/components/Predictor.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import * as tf from '@tensorflow/tfjs';
+
 
 
 import '../css/Predictor.css';
@@ -29,15 +29,30 @@ class Predictor extends Component {
     this.setState({ [colName]: value})
   }
 
-  fetchPrediction(){
-    return 1
+  fetchPrediction(e){
+    e.preventDefault()
+    fetch('http://127.0.0.1:8001/predict', {
+      method: 'post',
+      mode: 'no-cors',
+      body: JSON.stringify({
+        'age':this.state.age,
+        'sex':this.state.sex,
+        'cp':this.state.cp,
+        'trestbps':this.state.trestbps,
+        'thalach':this.state.thalach,
+        'exang':this.state.exang,
+        'oldpeak' :this.state.oldpeak,
+        'slope':this.state.slope,
+        'target':this.state.target
+      })
+    }).then(data=>JSON.stringify(data)).then((json)=>{
+      console.log("json recieved", json)
+    })
   }
   
   render(){
     return (
-      <div className='row predictor'>
-        <form action="" className='col s12'>
-          <div className='row'>
+          <div className='row col s12 predictor'>
             <form onSubmit={this.fetchPrediction}>
             <label htmlFor="age">Age: </label><input type="text" value={this.state.age} name='age' onChange={this.handleChange}/>
             <label htmlFor="sex">Sex: </label><input name="sex" type="text" value={this.state.sex} onChange={this.handleChange}/>
@@ -48,17 +63,14 @@ class Predictor extends Component {
               <option value="3">Non-Anginal Pain</option>
               <option value="4">Asymptomatic</option>
             </select>  
-            <label htmlFor="trestbps">Rest BPM: </label><input name="trestbps" type="text" value={this.state.trestbps} onChange={this.handleChange}/>
+            <label htmlFor="trestbps">Rest BPM: </label><input name="trestbps" type="text" value={this.state.trestbps} onChange={this.handleChange}/> 
             <label htmlFor="thalach">Max BPM: </label><input name="thalach" type="text" value={this.state.thalach} onChange={this.handleChange}/>
             <label htmlFor="exang">Exercised Induced Angina: </label><input name="exang" type="text" value={this.state.exang} onChange={this.handleChange}/>
             <label htmlFor="oldpeak">OldPeak: </label><input name="oldpeak" type="text" value={this.state.oldpeak} onChange={this.handleChange}/>
             <label htmlFor="slope">Slope: </label><input name="slope" type="text" value={this.state.slope} onChange={this.handleChange}/>
-            <input type="submit" value="What's My Prediction"/>
+            <input type="submit" onClick={this.fetchPrediction} value="What's My Prediction"/>
             </form>
           </div>
-
-        </form>
-      </div>
     )
   }
 }
