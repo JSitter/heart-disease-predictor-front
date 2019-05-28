3f6fa78eeea632ad09746f2be81f3f053f96689e
Justin Sitter <justin@jaytria.com>
2019-05-28 16:16:43 -0700

    Clarified field labels

--- src/components/Predictor.js
+++ src/components/Predictor.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 
-
-
 import '../css/Predictor.css';
 
 class Predictor extends Component {
   constructor(props){
-    super(props)
+    super(props);
     this.state = {
       'age':'',
       'sex':'',
@@ -17,22 +15,39 @@ class Predictor extends Component {
       'oldpeak' :'',
       'slope':'',
       'target':''
-    }
-    this.handleChange = this.handleChange.bind(this)
-    this.fetchPrediction = this.fetchPrediction.bind(this)
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.fetchPrediction = this.fetchPrediction.bind(this);
+    this.testCors = this.testCors.bind(this);
   }
 
   handleChange(event){
-    let colName = event.target.name
-    let value = event.target.value
+    let colName = event.target.name;
+    let value = event.target.value;
     
-    this.setState({ [colName]: value})
+    this.setState({ [colName]: value});
+  }
+
+  testCors(event){
+    event.preventDefault();
+    let url = "https://heart-disease-predictor.herokuapp.com/api/predict/";
+    fetch(url, {
+      mode: 'no-cors',
+      method: 'post'
+    }).then(response =>{
+      response.text().then(json=>console.log(json));
+    });
   }
 
   fetchPrediction(e){
-    e.preventDefault()
-    fetch('http://127.0.0.1:8001/predict', {
-      method: 'post',
+    e.preventDefault();
+    let backend_url = 'https://heart-disease-predictor.herokuapp.com/api/predict/';
+    fetch(backend_url, {
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
       mode: 'no-cors',
       body: JSON.stringify({
         'age':this.state.age,
@@ -45,15 +60,22 @@ class Predictor extends Component {
         'slope':this.state.slope,
         'target':this.state.target
       })
-    }).then(data=>JSON.stringify(data)).then((json)=>{
-      console.log("json recieved", json)
-    })
+    }).then((response)=>{
+      
+      if(response.status !== 200){
+        console.log("Error occured: ", response.status);
+      }else{
+        response.json().then(data=>console.log(JSON.stringify(data)));
+      }
+      
+      
+    }).catch(err=>console.log("Error occured: ", err));
   }
   
   render(){
     return (
           <div className='row col s12 predictor'>
-            <form onSubmit={this.fetchPrediction}>
+            <form>
             <label htmlFor="age">Age: </label><input type="text" value={this.state.age} name='age' onChange={this.handleChange}/>
             <label htmlFor="sex">Sex: </label><input name="sex" type="text" value={this.state.sex} onChange={this.handleChange}/>
             <label htmlFor="cp">Chest Pain Type: </label> 
@@ -63,15 +85,25 @@ class Predictor extends Component {
               <option value="3">Non-Anginal Pain</option>
               <option value="4">Asymptomatic</option>
             </select>  
-            <label htmlFor="trestbps">Rest BPM: </label><input name="trestbps" type="text" value={this.state.trestbps} onChange={this.handleChange}/> 
-            <label htmlFor="thalach">Max BPM: </label><input name="thalach" type="text" value={this.state.thalach} onChange={this.handleChange}/>
-            <label htmlFor="exang">Exercised Induced Angina: </label><input name="exang" type="text" value={this.state.exang} onChange={this.handleChange}/>
-            <label htmlFor="oldpeak">OldPeak: </label><input name="oldpeak" type="text" value={this.state.oldpeak} onChange={this.handleChange}/>
-            <label htmlFor="slope">Slope: </label><input name="slope" type="text" value={this.state.slope} onChange={this.handleChange}/>
+
+            <label htmlFor="trestbps">Resting Blood Pressure (mm Hg): </label>
+            <input name="trestbps" type="text" value={this.state.trestbps} onChange={this.handleChange}/> 
+            <label htmlFor="thalach">Max Heart Rate (Beats / Min): </label>
+            <input name="thalach" type="text" value={this.state.thalach} onChange={this.handleChange}/>
+            <label htmlFor="exang">Exercised Induced Angina: </label>
+            <input name="exang" type="text" value={this.state.exang} onChange={this.handleChange}/>
+            <label htmlFor="oldpeak">OldPeak: </label>
+            <input name="oldpeak" type="text" value={this.state.oldpeak} onChange={this.handleChange}/>
+            <label htmlFor="slope">
+              <i class="material-icons">info</i>
+              Upslope: 
+            </label>
+            <input name="slope" type="text" value={this.state.slope} onChange={this.handleChange}/>
+
             <input type="submit" onClick={this.fetchPrediction} value="What's My Prediction"/>
             </form>
           </div>
-    )
+    );
   }
 }
 
