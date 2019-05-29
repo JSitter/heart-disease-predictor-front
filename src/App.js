310558b8a8d361b76f4fd987f2fbd1ecf537d8d7
Justin Sitter <justin@jaytria.com>
2019-05-29 09:27:04 -0700

    Add useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Header from './components/Header';
 import NavBar from './components/NavBar';
 import Introduction from './components/Introduction';
