74286eff29f0b3bbd3a4b7e9bfaa04552693c0c8
Justin Sitter <justin@jaytria.com>
2019-06-10 13:01:03 -0700

    Change Access-control-allow-origin and allow-methods headers

--- src/components/Predictor.js
+++ src/components/Predictor.js
@@ -31,18 +31,35 @@ class Predictor extends Component {
     let colName = event.target.name;
     let value = event.target.value;
     this.setState({ [colName]: value});
+    console.log('State changed', this.state);
   }
 
   fetchPrediction(e){
     e.preventDefault();
+    console.log(this.state)
+    let required_params = [
+      'age',
+      'sex',
+      'cp',
+      'trestbps',
+      'thalach',
+      'exang',
+      'oldpeak',
+      'slope'
+    ]
+
+    
+
     let backend_url = 'https://heart-disease-backend.herokuapp.com/api/predict/';
     fetch(backend_url, {
       method: 'post',
-      mode: 'no-cors',
       cache: 'no-cache',
+      mode: 'no-cors',
       headers: {
         'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, HEAD'
       },
       body: JSON.stringify({
         'age':this.state.age,
@@ -71,47 +88,57 @@ class Predictor extends Component {
           <div className='row col s12 predictor'>
             {this.state.showResult ? <Result prediction={this.state.target}/> : null}
             <form>
-            <label htmlFor="age">Age: </label><input type="text" value={this.state.age} name='age' onChange={this.handleChange}/>
-            <label htmlFor="sex">Sex: 
-              <select name="sex" value={this.state.sex} onChange={this.handleChange}>
-                <option value="0">Female</option>
-                <option value="1">Male</option>
-              </select>
-            </label>
-            <label htmlFor="cp">
-              Chest Pain Type: {this.state.cp}
-              
-              <select name="cp" value={this.state.cp} onChange={this.handleChange}>
-                <option value="1">Typical Angina</option>
-                <option value="2">Atypical Angina</option>
-                <option value="3">Non-Anginal Pain</option>
-                <option value="4">Asymptomatic</option>
-              </select>  
-            </label>
-            
-            <label htmlFor="trestbps">Resting Blood Pressure (mm Hg): </label>
-            <input name="trestbps" type="text" value={this.state.trestbps} onChange={this.handleChange}/> 
-            <label htmlFor="thalach">Max Heart Rate (Beats / Min): </label>
-            <input name="thalach" type="text" value={this.state.thalach} onChange={this.handleChange}/>
-            <label htmlFor="exang">Exercise Induced Angina: 
-              <select name="exang" value={this.state.exang} onChange={this.handleChange}>
-                <option value="0">No</option>
-                <option value="1">Yes</option>
-              </select>
-            </label>
-            <label htmlFor="oldpeak">OldPeak (Between 0 and 6): 
-            <input name="oldpeak" type="text" value={this.state.oldpeak} onChange={this.handleChange}/>
-            </label>
-            <label htmlFor="slope">
-              Upslope: 
-              <select name="slope" value={this.state.slope} onChange={this.handleChange}>
-                <option value="1">Upsloping</option>
-                <option value="2">Flat</option>
-                <option value="3">Downsloping</option>
-              </select>
-            </label>
+              <label htmlFor="age">Age:
+                <input type="text" value={this.state.age} name='age' onChange={this.handleChange}/>
+              </label>
 
-            <input type="submit" onClick={this.fetchPrediction} value="What's My Prediction"/>
+              <label htmlFor="sex">Sex: 
+                <select name="sex" value={this.state.sex} onChange={this.handleChange}>
+                  <option value="0">Female</option>
+                  <option value="1">Male</option>
+                </select>
+              </label>
+
+              <label htmlFor="cp">
+                Chest Pain Type: {this.state.cp}
+                
+                <select name="cp" value={this.state.cp} onChange={this.handleChange}>
+                  <option value="1">Typical Angina</option>
+                  <option value="2">Atypical Angina</option>
+                  <option value="3">Non-Anginal Pain</option>
+                  <option value="4">Asymptomatic</option>
+                </select>  
+              </label>
+              
+              <label htmlFor="trestbps">Resting Blood Pressure (mm Hg):
+                <input name="trestbps" type="text" value={this.state.trestbsp} onChange={this.handleChange}/>
+              </label>
+
+              <label htmlFor="thalach">Max Heart Rate (Beats / Min):
+                <input name="thalach" type="text" value={this.state.thalach} onChange={this.handleChange}/>
+              </label>
+
+              <label htmlFor="exang">Exercise Induced Angina: 
+                <select name="exang" value={this.state.exang} onChange={this.handleChange}>
+                  <option value="0">No</option>
+                  <option value="1">Yes</option>
+                </select>
+              </label>
+
+              <label htmlFor="oldpeak">OldPeak (Between 0 and 6): 
+                <input name="oldpeak" type="text" value={this.state.oldpeak} onChange={this.handleChange}/>
+              </label>
+
+              <label htmlFor="slope">
+                Upslope: 
+                <select name="slope" value={this.state.slope} onChange={this.handleChange}>
+                  <option value="1">Upsloping</option>
+                  <option value="2">Flat</option>
+                  <option value="3">Downsloping</option>
+                </select>
+              </label>
+
+              <input type="submit" onClick={this.fetchPrediction} value="What's My Prediction"/>
             </form>
           </div>
     );
